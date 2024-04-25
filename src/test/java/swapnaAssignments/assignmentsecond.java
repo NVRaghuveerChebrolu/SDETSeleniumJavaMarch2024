1e1317c9dc9ce83665deeab9414e81db3f18bfbc
sjkoora <swapna374@gmail.com>
2024-04-24 20:29:47 -0500

    Changes made as per Review comment.

--- src/test/java/swapnaAssignments/assignmentsecond.java
+++ src/test/java/swapnaAssignments/assignmentsecond.java
@@ -1,24 +1,18 @@
 package swapnaAssignments;
 
+import java.io.IOException;
 
+import org.openqa.selenium.WebDriver;
+import com.utility.Library;
 
+public class assignmentsecond extends Library {
 
-		import java.io.IOException;
+	public static void main(String[] args) throws IOException {
 
-        import org.openqa.selenium.WebDriver;
-        import com.utility.Library;
-
-		public class assignmentsecond extends Library {
-		
-	    public static void main(String[] args) throws IOException {
-		     
-		  ReadPropertiesFile();
-		 LaunchBrowser(); 
+		ReadPropertiesFile();
+		LaunchBrowser();
 		driver.get(objProp.getProperty("walmartURL"));
 		PageLoadTimeOut();
-
-		 // driver.quit();
-		
 
 	}
 
