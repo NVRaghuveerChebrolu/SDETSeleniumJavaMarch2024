8989522ccf516cfebe8ea85992c937d5f1b4136b
Naga Venkata Raghuveer Chebrolu <raghuchnv@gmail.com>
2024-05-30 07:06:48 +0530

    jenkins changes in pom.xml

--- src/test/java/com/utility/Library.java
+++ src/test/java/com/utility/Library.java
@@ -4,27 +4,110 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class Library {
 	public static Properties objProp;
 	public static WebDriver driver;
 	HashMap<String, String> hmap = new HashMap<String, String>();
+	
+	public static ExtentHtmlReporter ExtHtmlReporter ;
+	public static ExtentReports ExtReports;
+	public static ExtentTest ExtTest;
+	
+	/*
+	 * ExtentHtmlReporter : responsible for look and feel of the report ,we can
+	 * specify the report name , document title , theme of the report
+	 * 
+	 * ExtentReports : used to create entries in your report , create test cases in
+	 * report , who executed the test case, environment name , browser
+	 * 
+	 * ExtentTest : update pass fail and skips and logs the test cases results
+	 */
+	
+	public void StartExtentReport() {
+		// TODO Auto-generated method stub
+		File objFile = new File(System.getProperty("user.dir")+"//test-output//ExtentReportV4.html");
+		ExtentHtmlReporter objExtentHtmlReporter = new ExtentHtmlReporter(objFile);
+		objExtentHtmlReporter.config().setDocumentTitle("Automation Report");
+		objExtentHtmlReporter.config().setReportName("Extent Report");
+		objExtentHtmlReporter.config().setTheme(Theme.DARK);
+		ExtReports = new ExtentReports();
+		ExtReports.attachReporter(objExtentHtmlReporter);
+		
+		ExtReports.setSystemInfo("TesterName", "Raghuveer");
+		ExtReports.setSystemInfo("Broswer", objProp.getProperty("Browser"));
+		ExtReports.setSystemInfo("Environment", "UAT");
+		
+	}
+	
+	public void UpdatingResultInExtentReport(ITestResult result) {
+		// TODO Auto-generated method stub
+		if(result.getStatus()==ITestResult.SUCCESS) {
+			ExtTest.log(Status.PASS, "Test Case Passed is "+result.getName());
+		}else if(result.getStatus()==ITestResult.FAILURE) {
+			ExtTest.log(Status.FAIL, "Test Case Failed is "+result.getName());
+			ExtTest.log(Status.FAIL, "Test Case Failed is "+result.getThrowable());
+			try {
+				ExtTest.addScreenCaptureFromPath(TakeScreenShot(result.getName()));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else if(result.getStatus()==ITestResult.SKIP) {
+			ExtTest.log(Status.SKIP, "Test Case Skipped is "+result.getName());
+		}
+	}
+	
+	/* Author : Raghuveer
+	 * This method is used to take screen shot and store the screen shots in side ScreenShot folder
+	 */
+	public static String TakeScreenShot(String testcaseName) throws IOException {
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String dateName = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+		String destination = System.getProperty("user.dir") + "//ScreenShots//" + dateName + testcaseName+"captured.jpeg";
+		FileUtils.copyFile(src, new File(destination));
+		return destination;
+	}
+	
+	public void FlushReport() {
+		ExtReports.flush();
+	}
+	
 	public static void ReadPropertiesFile() throws IOException {
 		System.out.println("path where project is stored:"+System.getProperty("user.dir"));
 		try {
@@ -38,6 +121,51 @@ public class Library {
 		}
 		
 	}
+	
+	//below is the method for selenium 4
+		public WebDriver getBrowserCapabilities(String BrowserName) {
+			if (BrowserName == null || BrowserName.equalsIgnoreCase("FIREFOX")) {
+				FirefoxOptions options = new FirefoxOptions();
+				// options.setHeadless(headless);
+				try {
+					driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else if (BrowserName.equalsIgnoreCase("IE")) {
+				// capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,
+				// true);
+				// capabilities.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION,
+				// true);
+				InternetExplorerOptions options = new InternetExplorerOptions();
+				try {
+					driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else if (BrowserName.equalsIgnoreCase("CHROME")) {
+				ChromeOptions options = new ChromeOptions();
+				// options.setHeadless(headless);
+				try {
+					driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else if (BrowserName.equalsIgnoreCase("EDGE")) {
+				EdgeOptions options = new EdgeOptions();
+				try {
+					driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+			return driver;
+		}
 	
 	public static void LaunchBrowser() {
 		String browerName = objProp.getProperty("browser");
@@ -60,10 +188,31 @@ public class Library {
 		}
 		driver.manage().window().maximize();
 		//implicit wait : It is a global waiting mechanism which is applicable for all web elements 
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		
-		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));	
 	}
+	
+	public WebDriver initializeBrowser(String broswerName) {
+		DesiredCapabilities dc = new DesiredCapabilities();
+		if (broswerName.equals("chrome")) {
+			dc.setBrowserName("chrome");
+		} else if (broswerName.equals("firefox")) {
+			dc.setBrowserName("firefox");
+		} else if (broswerName.equals("safari")) {
+			dc.setBrowserName("safari");
+		} else if (broswerName.equals("Edge")) {
+			dc.setBrowserName("Edge");
+		} else if (broswerName.equals("ie")) {
+			dc.setBrowserName("ie");
+		}
+		try {
+			driver = new RemoteWebDriver(new URL("http://localhost:4444"), dc);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return driver;
+	}
+
 	
 	public static void PageLoadTimeOut() {
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Constants.pageLoadTimeout));
@@ -72,10 +221,7 @@ public class Library {
 	public void scrollIntoView(WebElement element) {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].scrollIntoView(true);",element);
-
 	}
-	
-	
 	
 	public HashMap<String, String> ReadExcelTestDataFile(XSSFSheet objXSSFSheet, int row) {
 		// TODO Auto-generated method stub
@@ -108,9 +254,7 @@ public class Library {
 	}
 
 	public void WriteToExcelFile(int row, XSSFSheet objXSSFSheet) {
-
 		objXSSFSheet.getRow(row).createCell(18).setCellValue("Pass");
-
 	}
 	
 	public void SelectValueFromDropDown(List<WebElement> AllDropDownItems, String ExpectedDropDownValue) {
